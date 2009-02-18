8a31231b6e9a23032fd29161d5c4ee180b5d97cc
Joas Schilling <nickvergessen@gmx.de>
2009-02-18 01:02:53 +0000

    MODx 1.2.0 > 1.2.2 phpBB 3.0.2 > 3.0.4

--- root/adm/mods/nv_usertopics_version.php
+++ root/adm/mods/nv_usertopics_version.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package acp
-* @version $Id: mod_version_check_version.php 51 2007-10-30 04:40:42Z Handyman $
+* @version $Id$
 * @copyright (c) 2007 StarTrekGuide
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
@@ -25,7 +25,7 @@ class nv_usertopics_version
 			'author'	=> 'nickvergessen',
 			'title'		=> 'NV usertopics',
 			'tag'		=> 'nv_usertopics',
-			'version'	=> '1.0.0',
+			'version'	=> '1.0.1',
 			'file'		=> array('www.flying-bits.org', 'updatecheck', 'nv_usertopics.xml'),
 		);
 	}
